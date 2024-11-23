c7242cece8f7eedc0752a7467136132d405abaf9
Goncalo <gnunosilva@hotmail.com>
2024-11-23 13:25:22 +0100

    fix build error

--- app/feedback/page.tsx
+++ app/feedback/page.tsx
@@ -1,5 +1,4 @@
 import FeedbackComponent from "@/components/feedback-page";
-import { withAuth } from "@/components/withAuth";
 
 export default function FeedbackPage() {
   return <FeedbackComponent />
